d3553b89d7452e70d5526ccef0808a3c32b57688
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-06-13 17:02:09 +0530

    add navigation bar

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Logo from "../public/assests/logo.png"
+import PImg1 from "../public/assests/img1.png"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,8 +21,29 @@ export default function Home() {
         </style>
 
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <header>
+      <nav className='flex justify-between'>
         
+        <Image
+          src={Logo}
+          alt="logo"
+          width='85'
+          height='10'
+          className="my-8 mx-10 inline-block"
+        />
+        <ul className='font-raleway font-semibold  flex justify-end mx-16 my-12'>
+          <li className='mx-8'>HOME</li>
+          <li className='mx-8'>ABOUT ME</li>
+          <li className='mx-8'>SKILS</li>
+          <li className='mx-8'>PROJECTS</li>
+          <li className='mx-8'>CONTACT</li>
+        </ul>
+        
+       
+       </nav>
+      </header>
+      <main >
+      
       </main>
     </>
   )
