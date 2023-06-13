2c2e5b3b10c1717c12e6cfcfcad5bf94e25ef7f5
HansikaPerera1117 <hansikaperera59@gmail.com>
2023-06-13 16:16:32 +0530

    add fonts and add meta tags

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,13 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        ubuntu:["Ubuntu","sans-serif"],
+        lobster:["Lobster","cursive"],
+        raleway:["Raleway","sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
